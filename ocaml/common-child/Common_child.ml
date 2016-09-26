30aaebca4173a3c6dce0137306b4cb5d5cd98a39
Steve <stevejb71@gmail.com>
2016-09-26 19:05:37 +0800

    common-child revisited

--- ocaml/common-child/Common_child.ml
+++ ocaml/common-child/Common_child.ml
@@ -1,16 +1,18 @@
 open Core.Std
 
+let nested_string_iteri (s1: string) (s2: string) (f: int -> char -> int -> char -> unit): unit =
+  for i1 = 0 to String.length s1 - 1 do
+    for i2 = 0 to String.length s2 - 1 do
+      f i1 s1.[i1] i2 s2.[i2]
+    done
+  done;;
+        
 let common_child (s1: string) (s2: string): int =
-  let rec go (s1: string) (s2: string) (result: int): int =
-    if String.is_empty s1 then result
-    else match String.index s2 s1.[0] with
-      | None -> go (String.drop_prefix s1 1) s2 result
-      | Some(i2) ->
-         let m1 = go (String.drop_prefix s1 1) (String.drop_prefix s2 (i2+1)) (1 + result) in
-         let m2 = go (String.drop_prefix s1 1) s2 result in
-         max m1 m2
-  in go s1 s2 0
-
+  let m = Array.make_matrix ~dimx:(1 + String.length s1) ~dimy:(1 + String.length s2) 0 in
+  nested_string_iteri s1 s2 (fun i1 c1 i2 c2 ->
+                        m.(i1+1).(i2+1) <- if c1 = c2 then m.(i1).(i2) + 1 else max m.(i1+1).(i2) m.(i1).(i2+1));
+  m.(String.length s1).(String.length s2);;
+                    
 let () =
   let strs = Array.of_list (In_channel.input_lines stdin) in
   let s1 = strs.(0) in
